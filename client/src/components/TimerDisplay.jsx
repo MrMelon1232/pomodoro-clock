dc0ea0f0592a2c9daa4f8d8c5e2788e1a35f7f5c
Julien Phan <julien.phan18@gmail.com>
2025-10-25 02:24:40 -0400

    Added styling to the timer display component
    Styling includes sizing for responsive design, WIP primary and secondary buttons for mode changes and start/pause as well as a reset timer icon button

--- client/src/components/TimerDisplay.jsx
+++ client/src/components/TimerDisplay.jsx
@@ -3,28 +3,47 @@ import { usePomodoro } from "../hooks/usePomodoro";
 import { MODE_STYLES } from "../themes/modeStyles";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { SecondaryButton } from "../components/SecondaryButton";
+import { LuTimerReset } from "react-icons/lu";
+import { PrimaryButton } from "../components/PrimaryButton";
 
 export const TimerDisplay = (settings) => {
-    const { mode, formattedTime, progress, start, stop, reset, changeMode } =
-        usePomodoro(settings);
+    const {
+        mode,
+        formattedTime,
+        isRunning,
+        progress,
+        start,
+        stop,
+        reset,
+        changeMode,
+    } = usePomodoro(settings);
 
     const style = MODE_STYLES[mode];
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-center items-center w-full py-6">
             {/* Mode change options */}
-            <div className="">
-                <button onClick={() => changeMode(MODES.WORK)}>Pomodoro</button>
-                <button onClick={() => changeMode(MODES.SHORT_BREAK)}>
-                    Shot Break
-                </button>
-                <button onClick={() => changeMode(MODES.LONG_BREAK)}>
-                    Long Break
-                </button>
+            <div className="grid grid-cols-3 gap-4 py-6">
+                <SecondaryButton
+                    label="Pomodoro"
+                    onClick={() => changeMode(MODES.WORK)}
+                    active={mode === MODES.WORK}
+                />
+                <SecondaryButton
+                    label="Short Break"
+                    onClick={() => changeMode(MODES.SHORT_BREAK)}
+                    active={mode === MODES.SHORT_BREAK}
+                />
+                <SecondaryButton
+                    label="Long Break"
+                    onClick={() => changeMode(MODES.LONG_BREAK)}
+                    active={mode === MODES.LONG_BREAK}
+                />
             </div>
 
             {/* Circular Progression bar  */}
-            <div className="">
+            <div className="w-56 sm:w-64 md:w-72 lg:w-1/2">
                 <CircularProgressbar
                     value={progress}
                     text={formattedTime}
@@ -32,15 +51,30 @@ export const TimerDisplay = (settings) => {
                         pathColor: style.color,
                         textColor: style.color,
                         trailColor: "#1f2937",
+                        textSize: "16px",
+                        strokeLinecap: "round",
+                        pathTransitionDuration: 0.5,
                     })}
                 ></CircularProgressbar>{" "}
             </div>
 
             {/* Start, Pause, Reset buttons */}
-            <div className="">
-                <button onClick={start}>Start</button>
-                <button onClick={stop}>Pause</button>
-                <button onClick={reset}>Reset</button>
+            <div className="flex justify-center items-center gap-4 mt-6">
+                {!isRunning ? (
+                    <PrimaryButton
+                        label="Start"
+                        color="terracotta"
+                        onClick={start}
+                    />
+                ) : (
+                    <PrimaryButton
+                        color="terracotta"
+                        label="Pause"
+                        onClick={stop}
+                    />
+                )}
+
+                <LuTimerReset className="size-10 " onClick={reset} />
             </div>
         </div>
     );
