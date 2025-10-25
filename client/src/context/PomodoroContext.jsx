8cd60a85752773fcd14516b4f84547e18a9c4b24
Julien Phan <julien.phan18@gmail.com>
2025-10-25 18:49:23 -0400

    Modified our functions in our pomodoro context based on our order attribute in our list of tasks
    addTask, removeTask and completePomodoroForTask have been modified to accommodate the new order attribute added to tasks.

--- client/src/context/PomodoroContext.jsx
+++ client/src/context/PomodoroContext.jsx
@@ -18,6 +18,7 @@ export const PomodoroProvider = ({ children }) => {
             requiredPomodoros,
             completedPomodoros: 0,
             done: false,
+            order: tasks.length + 1,
         };
 
         // Append to our previous state our new task
@@ -26,7 +27,16 @@ export const PomodoroProvider = ({ children }) => {
 
     // Function to remove tasks
     const removeTask = (taskId) => {
-        setTasks((prev) => prev.filter((task) => task.id !== taskId));
+        setTasks((prev) => {
+            // Filter out the task we want to remove
+            const filtered = prev.filter((task) => task.id !== taskId);
+
+            // Reorder our list of tasks
+            return filtered.map((task, index) => ({
+                ...task,
+                order: index + 1,
+            }));
+        });
     };
 
     // Function to mark a completed pomodoro for a task
@@ -44,6 +54,28 @@ export const PomodoroProvider = ({ children }) => {
                     : task
             )
         );
+
+        // queue the next tasks as active if the current one is completed
+        const updated = tasks.find((t) => t.id == activeTask.id);
+        if (activeTask && updated.done) queueNextTask();
+    };
+
+    // Function to determine the next tasks based on order
+    const queueNextTask = () => {
+        setActiveTask((prev) => {
+            if (!prev) return null;
+
+            // Check if the next tasks based on order exists in our list of tasks
+            const nextTask = tasks.find(
+                (t) => t.order > prev.order + 1 && !t.done
+            );
+            return nextTask || null;
+        });
+    };
+
+    const manuallySetActiveTask = (taskId) => {
+        const task = tasks.find((t) => t.id === taskId);
+        if (task) setActiveTask(task);
     };
 
     return (
@@ -53,7 +85,7 @@ export const PomodoroProvider = ({ children }) => {
                 addTask,
                 removeTask,
                 activeTask,
-                setActiveTask,
+                setActiveTask: manuallySetActiveTask,
                 completePomodoroForTask,
             }}
         >
