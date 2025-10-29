84ef8f3446973e1f374e12629dd3fbf77052a897
Julien Phan <julien.phan18@gmail.com>
2025-10-29 19:17:24 -0400

    Added the task list to our app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
-import { TimerDisplay } from "./components/TimerDisplay";
+import { TimerDisplay } from "./components/timer/TimerDisplay";
+import { TaskList } from "./components/tasks/TaskList";
+import { Drawer } from "./components/common/Drawer";
+import { DrawerButton } from "./components/buttons/DrawerButton";
+import { useState } from "react";
 
 function App() {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
     return (
         <div>
             <TimerDisplay />
+
+            <DrawerButton onClick={() => setIsDrawerOpen(true)} />
+            <Drawer
+                isOpen={isDrawerOpen}
+                onClose={() => setIsDrawerOpen(false)}
+            >
+                <TaskList />
+            </Drawer>
         </div>
     );
 }
