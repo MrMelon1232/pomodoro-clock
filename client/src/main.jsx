4a1c0706f47b4b1ddbf953ebc8f8a517d5f5240d
Julien Phan <julien.phan18@gmail.com>
2025-11-02 15:23:39 -0500

    Wrapped app within settings context and provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { PomodoroProvider } from "./context/PomodoroContext";
+import { SettingsProvider } from "./context/SettingsContext";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <PomodoroProvider>
-            <App />
-        </PomodoroProvider>
+        <SettingsProvider>
+            <PomodoroProvider>
+                <App />
+            </PomodoroProvider>
+        </SettingsProvider>
     </StrictMode>
 );
