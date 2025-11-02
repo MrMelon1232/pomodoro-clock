ddf213b88b8f4ea694f4d5c3d81ecb578ac4b3dd
Julien Phan <julien.phan18@gmail.com>
2025-11-02 15:22:45 -0500

    Created reusable toggle button

---
+++ client/src/components/buttons/ToggleButton.jsx
@@ -0,0 +1,11 @@
+// A reuseable toggle button
+export const Toggle = ({ enabled, onChange }) => {
+    return (
+        <button
+            onClick={() => onChange(!enabled)}
+            className={`relative inline-flex h-6 w-11 items-center rounded-full transition ${
+                enabled ? "bg-green-500" : "bg-gray-400"
+            }`}
+        ></button>
+    );
+};
