067db25e266a01523c462de81c0bc818fbb20e58
Julien Phan <julien.phan18@gmail.com>
2025-11-02 15:22:22 -0500

    Added reusable dropdown with entry button

---
+++ client/src/components/buttons/DropdownButton.jsx
@@ -0,0 +1,19 @@
+// A reuseable dropdown with entry button
+export const Select = ({ label, value, onChange, options }) => {
+    return (
+        <label className="flex flex-col gap-1">
+            {label}
+            <select
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                className="border rounded px-2 py-1"
+            >
+                {options.map((opt) => (
+                    <option key={opt} value={opt}>
+                        {opt}
+                    </option>
+                ))}
+            </select>
+        </label>
+    );
+};
