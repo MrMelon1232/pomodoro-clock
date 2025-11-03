a09bf686f42d494044d56c436d9135ddf8fb9ee4
Julien Phan <julien.phan18@gmail.com>
2025-11-03 14:00:46 -0500

    Updated main UI interface to include a settings button and layout for other features to come

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,54 @@
 import "./App.css";
+import { useState } from "react";
 import { TimerDisplay } from "./components/timer/TimerDisplay";
 import { TaskList } from "./components/tasks/TaskList";
 import { Drawer } from "./components/common/Drawer";
 import { DrawerButton } from "./components/buttons/DrawerButton";
-import { useState } from "react";
+import { FiSettings } from "react-icons/fi";
+import { Settings } from "./components/settings/Settings";
 
 function App() {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [isTaskDrawerOpen, setIsTaskDrawerOpen] = useState(false);
+    const [isSettingsDrawerOpen, setIsSettingsDrawerOpen] = useState(false);
 
     return (
         <div>
             <TimerDisplay />
 
-            <DrawerButton onClick={() => setIsDrawerOpen(true)} />
-            <Drawer
-                isOpen={isDrawerOpen}
-                onClose={() => setIsDrawerOpen(false)}
-            >
-                <TaskList />
-            </Drawer>
+            {/* Container task drawer, settings, background changes and sound changes */}
+            <div className="grid grid-cols-3 gap-4">
+                {/* Background, sound preferences */}
+                <div> dasdas</div>
+
+                {/* Task drawer */}
+                <div className="flex justify-center">
+                    <DrawerButton
+                        label="Check your Tasks!"
+                        onClick={() => setIsTaskDrawerOpen(true)}
+                    />
+                    <Drawer
+                        isOpen={isTaskDrawerOpen}
+                        onClose={() => setIsTaskDrawerOpen(false)}
+                    >
+                        <TaskList />
+                    </Drawer>
+                </div>
+
+                {/* Settings */}
+                <div className="flex justify-center">
+                    <FiSettings
+                        size={24}
+                        className="cursor-pointer"
+                        onClick={() => setIsSettingsDrawerOpen(true)}
+                    />
+                    <Drawer
+                        isOpen={isSettingsDrawerOpen}
+                        onClose={() => setIsSettingsDrawerOpen(false)}
+                    >
+                        <Settings />
+                    </Drawer>
+                </div>
+            </div>
         </div>
     );
 }
