ed547b9ad684c0b4257035ebd98d50d6898c9964
Julien Phan <julien.phan18@gmail.com>
2025-11-02 19:54:51 -0500

    Added missing toggle to the toggle button

--- client/src/components/buttons/ToggleButton.jsx
+++ client/src/components/buttons/ToggleButton.jsx
@@ -2,10 +2,20 @@
 export const ToggleButton = ({ enabled, onChange }) => {
     return (
         <button
+            type="button"
             onClick={() => onChange(!enabled)}
-            className={`relative inline-flex h-6 w-11 items-center rounded-full transition ${
-                enabled ? "bg-green-500" : "bg-gray-400"
-            }`}
-        ></button>
+            className={`relative inline-flex h-6 w-11 rounded-full transition-colors 
+                duration-300 focus:outline-none focus:ring-2 focus:ring-terracotta
+                ${enabled ? "bg-terracotta" : "bg-gray-500"}
+            `}
+        >
+            <span
+                className={`
+                    inline-block h-5 w-5 transform rounded-full bg-white shadow 
+                    transition-transform duration-300
+                    ${enabled ? "translate-x-5" : "translate-x-1"}
+                `}
+            />
+        </button>
     );
 };
